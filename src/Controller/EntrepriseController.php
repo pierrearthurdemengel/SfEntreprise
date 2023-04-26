26bd65b03a2c3f18c86ce2eff2d34c8310ad55b4
pierrearthurdemengel <pademengel@gmail.com>
2023-04-26 14:41:59 +0200

    .

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -23,76 +23,73 @@ class EntrepriseController extends AbstractController
     public function index(ManagerRegistry $doctrine): Response
     {
         // récupérer les entreprises de la bdd
-        $entreprises = $doctrine->getRepository (Entreprise::class)->findAll();
+        $entreprises = $doctrine->getRepository(Entreprise::class)->findAll();
         $tableau = ["valeur 1", "valeur 2", "valeur 3", "valeur 4"];
         return $this->render('entreprise/index.html.twig', [
-    'entreprises' => $entreprises
+            'entreprises' => $entreprises
         ]);
     }
-    
+
     #[Route('/entreprise/add', name: 'add_entreprise')]
     #[Route('/entreprise/{id}/edit', name: 'edit_entreprise')]
- 
-public function add(ManagerRegistry $doctrine, Entreprise $entreprise = null, Request $request): Response 
-{
-    // Si l'entreprise n'existe pas
-    if(!$entreprise) {
-        $entreprise = new Entreprise();
-    }
-    // construit un formulaire qui se repose sur le $builder dans EntrepriseType 
-    $form = $this->createForm(EntrepriseType::class, $entreprise);
-    // analyse de ce qui se passe dans mon form
-    $form->handleRequest($request);
 
-    if($form->isSubmited() && $form->isValid()) 
+    public function add(ManagerRegistry $doctrine, Entreprise $entreprise = null, Request $request): Response
     {
-        // récupère les données saisient dans le form et ça les inject (setter), ça les hydrate = donne des valeurs
-        $entreprise = $form->getData();
-        $entityManager = $doctrine->getManager();
-        // retiens l'objet en mémoire (prepare)
-        $entityManager->persist($entreprise);
-        // flush = tirer la chasse d'eau = envoye des données à la BDD (insert into (execute))
-        $entityManager->flush();
+        // Si l'entreprise n'existe pas
+        if (!$entreprise) {
+            $entreprise = new Entreprise();
+        }
+        // construit un formulaire qui se repose sur le $builder dans EntrepriseType 
+        $form = $this->createForm(EntrepriseType::class, $entreprise);
+        // analyse de ce qui se passe dans mon form
+        $form->handleRequest($request);
 
-        return $this->redirectToRoute('app_entreprise');
+        if ($form->isSubmited() && $form->isValid()) {
+            // récupère les données saisient dans le form et ça les inject (setter), ça les hydrate = donne des valeurs
+            $entreprise = $form->getData();
+            $entityManager = $doctrine->getManager();
+            // retiens l'objet en mémoire (prepare)
+            $entityManager->persist($entreprise);
+            // flush = tirer la chasse d'eau = envoye des données à la BDD (insert into (execute))
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_entreprise');
+        }
+
+        // vue formulaire add
+        return $this->render('entreprise/add.html.twig', [
+            // create view = génère la vue du formulaire
+            'formAddEntreprise' => $form->createView(),
+            'edit' => $entreprise->getId()
+        ]);
     }
-    
-    // vue formulaire add
-    return $this->render('entreprise/add.html.twig', [
-        // create view = génère la vue du formulaire
-        'formAddEntreprise' => $form->createView(),
-        'edit' => $entreprise->getId()
-    ]);  
-}
 
 
     #[Route('/entreprise/{id}/delete', name: 'delete_entreprise')]
 
     public function delete(ManagerRegistry $doctrine, Entreprise $entreprise): Response
-{
-    $entityManger = $doctrine->getManager();
-    $entityManager = remove($entreprise);
-    $entityManager->flush();
+    {
+        $entityManger = $doctrine->getManager();
+        $entityManager = remove($entreprise);
+        $entityManager->flush();
 
-    return $this->redirectToRoute('app_entreprise');
-}
-
-
-
-
-
-
-
-
-
-#[Route('/entreprise/{id}', name: 'show_entreprise')]
-
-public function show(Entreprise $entreprise): Response
-{
-    return $this->render('entreprise/show.html.twig', [
-            'entreprise' => $entreprise
-        ]);
+        return $this->redirectToRoute('app_entreprise');
     }
 
 
+
+
+
+
+
+
+
+    #[Route('/entreprise/{id}', name: 'show_entreprise')]
+
+    public function show(Entreprise $entreprise): Response
+    {
+        return $this->render('entreprise/show.html.twig', [
+            'entreprise' => $entreprise
+        ]);
+    }
 }
